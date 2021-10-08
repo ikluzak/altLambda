693d9f88c7ce0eb80e1984e10be4341ca454920c
Ivan Kluzak <sooper@gmail.com>
2021-10-08 17:06:38 -0500

    cleanup and comments in index.js

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
-const express   	= require('express');
-const fs		= require('fs');
-const vm4		= require('./vm4.js');
-const uuid		= require('uuid').v4;
-const AWS		= require('aws-sdk');
+//
+// File:    index.js
+// Author:  Ivan Kluzak
+// Date:    10/08/2021
+// Notes:   altLambda - something to do a little local testing of an API-gw-like environment
+//          mapped to lambda files and execute it locally.
+//  
+//          Also attempt to create a logs folder somewhat similar to what you might expect
+//
+//          Also attempt to provide credentials to the environment of the function executed
+//          set via a temporary token fetched using the specified profile name.
+//
+//          Only planning to support Lambda proxy mode at the moment
+//
+const express       = require('express');
+const fs            = require('fs');
+const vm4           = require('./vm4.js');
+const uuid          = require('uuid').v4;
+const AWS           = require('aws-sdk');
 
-const app 		= express();
-const port 		= 8000;
-const AWS_PROFILE	= 'default';
+const app           = express();
+//
+// ----------------------------------------------------------------------------------------------
+// CONFIGURATION:
 
+const port          = 8000;         // <------ SET THIS TO YOUR PREFERRED
+const AWS_PROFILE   = 'default';    // <------ SET THIS TO YOUR PREFERRED
+
+
+
+
+// ----------------------------------------------------------------------------------------------
+
+// Disable this information...
 app.disable('x-powered-by');
+
+// By default we'll just handle OPTIONS for anything...
 app.options("/*", function(req, res, next) {
-  console.log(`API-gw(OPTIONS)`);
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.send(200);
+    console.log(`API-gw(OPTIONS)`);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
 });
 
+// getTempCreds()
+//  - pass the profile you want to use
+//  - fetch temporary creds so we aren't using that profile directly...
+//  - return those temporary creds, so later we can pass into the environment of the node vm context
 async function getTempCreds(profile) {
 
-	var _AWS 		= require('aws-sdk');
-	var credentials         = new _AWS.SharedIniFileCredentials({profile: profile});
-	_AWS.config.credentials = credentials;
-	var sts 		= new _AWS.STS({apiVersion: '2011-06-15'});
+    const _AWS              = require('aws-sdk');
+    _AWS.config.credentials = new _AWS.SharedIniFileCredentials({profile: profile});
+    var sts                 = new _AWS.STS({apiVersion: '2011-06-15'});
 
-	var c 			= await sts.getSessionToken().promise();
+    var c                   = await sts.getSessionToken().promise();
 
 	return {
 		id : c.Credentials.AccessKeyId,
@@ -33,151 +62,149 @@ async function getTempCreds(profile) {
 	};
 }
 
-
+//
+// Class to store our lambda specific configuration
 class APIgw {
 
-	constructor(lambda, method, path) {
-		this.method = method;
-		this.path = path;
-		this.lambda = lambda;
-		this.self   = this;
-	}
+    constructor(lambda, method, path) {
+        this.method = method;
+        this.path   = path;
+        this.lambda = lambda;
+        this.self   = this;
+    }
 
-	async functionHandler(req, res) {
+    // Do the function execution step...
+    async functionHandler(req, res) {
 
-		var that = this.self;
+        var that = this.self;
+        console.log(`API-gw(${that.method})->${that.lambda}`);
 
-        	console.log(`API-gw(${that.method})->${that.lambda}`);
+        try {
 
-		try {
-			var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-			var qs = null;
-			if (JSON.stringify(req.query) !== "{}") {
-				qs = req.query;
-			}
+            // Get the client IP to pass in later
+            var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+            var qs = null;                                  // default to null
+            if (JSON.stringify(req.query) !== "{}") {       // Otherwise, set to the query params
+                qs = req.query;
+            }
 
-			var key = getTempCreds(AWS_PROFILE);			// TODO: should we cache this?  Can we cache this? 
-			process.env = {};
-			process.env.foo = "bar";
-			process.env.AWS_ACCESS_KEY_ID = key.id;			// Kludge to give credentials to the script
-			process.env.AWS_SECRET_ACCESS_KEY = key.secret;
-			process.env.AWS_SESSION_TOKEN = key.token;
+            var key = getTempCreds(AWS_PROFILE);			// TODO: should we cache this?  Can we cache this? 
+            process.env = {};
+            // process.env.foo = "bar";                     // TODO: provide a mechanism to define ENV parameters to the lambda
+            process.env.AWS_ACCESS_KEY_ID = key.id;			// Kludge to give credentials to the lambda environment
+            process.env.AWS_SECRET_ACCESS_KEY = key.secret;
+            process.env.AWS_SESSION_TOKEN = key.token;
 
+            //
+            // Call the node VM
+            var out = await vm4(that.lambda, {
+                resource: that.path,
+                path: that.path,
+                httpMethod: that.method,
+                headers: req.headers,
+                //multiValueHeaders : {}                    // TODO: handle these
+                queryStringParameters : qs,
+                //pathParameters : {}                       // TODO: handle these
+                stageVariables: null,                       // TODO: mock or support cfg for this
+                requestContext : {
+                    resourceId : 'bogus',                   // TODO: we 'could' associate this with our defined one on the cfg file
+                    resourcePath: that.path,
+                    httpMethod : that.method,
+                    extendedRequestId: 'bogus',             // TODO: mock or support cfg 
+                    requestTime: (new Date().toString()),	// TODO: fix this formatting... 
+                    path : that.path,
+                    accountId: '11111111111',               // TODO: allow cfg
+                    protocol: "HTTP/1.1",                   // TODO: can we detect this from express? 
+                    stage: "v1",                            // TODO: allow to be mocked
+                    domainPrefix: 'mydomain',               // TODO: allow to be mocked
+                    requestTimeEpoch: (new Date().getTime()),// TODO: does this need to be unixtime or js time? 
+                    requestId: uuid(),                      // does this need to match something else?  or is this fine
+                    identity: {
+                        cognitoIdentityPoolId: null,        // allow for mock
+                        cognitoIdentityId : null,
+                        apiKey : 'allow for mock',          // TODO: mock
+                        principalOrgId : null,
+                        cognitoAuthenticationType: null,
+                        userArn : null,
+                        userAgent: null,
+                        accountId: null,
+                        caller: null,
+                        sourceIp: ip,
+                        accessKey : null,
+                        cognitoAuthenticationProvider : null,
+                        user : null
+                    },
+                    domainName : 'mydomain.localhost',      // TODO: allow for mock	
+                    apiId: 'bogus',                         // TODO: mock
+                },
+                body : undefined,                           // TODO: support this? 
+                isBase64Encoded : false                     // TODO: support this
+            });
 
-			var out = await vm4(that.lambda, {
-				resource: that.path,
-				path: that.path,
-				httpMethod: that.method,
-				headers: req.headers,
-				//multiValueHeaders : {}			// TODO
-				queryStringParameters : qs,
-				//pathParameters : {}				// TODO
-				stageVariables: null,				// TODO
-				requestContext : {
-					resourceId : 'bogus',			// TODO: we 'could' associate this with our defined one on the cfg file
-					resourcePath: that.path,
-					httpMethod : that.method,
-					extendedRequestId: 'bogus',		// TODO: fill this in? 
-					requestTime: (new Date().toString()),	// TODO: fix this formatting... 
-					path : that.path,
-					accountId: '11111111111',		// Whatever, it's our bogus one :D
-					protocol: "HTTP/1.1",			// TODO: can we detect this from express? 
-					stage: "v1",				// TODO: allow to be mocked
-					domainPrefix: 'mydomain',		// TODO: allow to be mocked
-					requestTimeEpoch: (new Date().getTime()), // TODO: does this need to be unixtime or js time? 
-					requestId: uuid(),			// 
-					identity: {
-						cognitoIdentityPoolId: null,	// allow for mock
-						cognitoIdentityId : null,
-						apiKey : 'allow for mock',	// 
-						principalOrgId : null,
-						cognitoAuthenticationType: null,
-						userArn : null,
-						userAgent: null,
-						accountId: null,
-						caller: null,
-						sourceIp: ip,
-						accessKey : null,
-						cognitoAuthenticationProvider : null,
-						user : null
-					},
-					domainName : 'mydomain.localhost',	// TODO: allow for mock	
-					apiId: 'bogus',				// mock
-				},
-				body : undefined,				// TODO: support this? 
-				isBase64Encoded : false				// suppor tthis
-			});
+            //
+            // Start sending back our results... 
+            res.status(out.statusCode);                     // What was the status code?
+            if (typeof out.headers !== 'undefined') {       // Any headers?
+                res.set(out.headers);
+            }
+            res.send(out.body);                             // Message body
 
-			//console.log(`sending`, JSON.stringify(out));
-			res.status(out.statusCode);
-			if (typeof out.headers !== 'undefined') {
-				res.set(out.headers);
-			}
-			res.send(out.body);
-
-		} catch (e) {
-			var msg = {
-				statusCode: 500,
-				body: e.toString()
-			}
-			res.send(JSON.stringify(msg));
-		}
-	}
+        } catch (e) {
+            // If we had some kind of crash, let's try to respond meaningfully...
+            var msg = {
+                statusCode: 500,
+                body: e.toString()
+            }
+            res.send(JSON.stringify(msg));
+        }
+    }
 }
 
+//
+// Load the altLambda configuration file:
 function loadCfg(fname, app) {
 
-	console.log(`API-gw()->loading configuration...`);
+    console.log(`API-gw()->loading configuration...`);
 
-	try {
+    try {
+        var cfg = fs.readFileSync(fname);
+        cfg = JSON.parse(cfg.toString());	
+    } catch (e) {
+        console.log(`altLambda->ERROR: in configuration file`);
+        console.log(e);
+    }
 
-		var cfg = fs.readFileSync(fname);
-		cfg = JSON.parse(cfg.toString());	
+    //
+    // Configure express...
+    try {
 
-	} catch (e) {
-		console.log(e);
-	}
+        for (var i=0; i < cfg.length; i++) {
+            console.log(`\tmethod: `, cfg[i].method);
+            console.log(`\tpath:   `, cfg[i].path);
+            console.log(`\tlambda: `, cfg[i].lambda);
 
-	//
-	// Configure express...
+            if (cfg[i].method === 'GET') {
+                // Create a new APIgw object and map to route in express:
+                var api = new APIgw(cfg[i].lambda, cfg[i].method, cfg[i].path);
+                app.get(cfg[i].path, (req, res) => { api.functionHandler(req,res); } );
 
-	try {
-
-	    for (var i=0; i < cfg.length; i++) {
-		console.log(`\tmethod: `, cfg[i].method);
-		console.log(`\tpath:   `, cfg[i].path);
-		console.log(`\tlambda: `, cfg[i].lambda);
-
-		if (cfg[i].method === 'GET') {
-
-			var api = new APIgw(cfg[i].lambda, cfg[i].method, cfg[i].path);
-			app.get(cfg[i].path, (req, res) => { api.functionHandler(req,res); } );
-
-		} else if (cfg[i].method === 'POST') {
-
-
-
-		} else if (cfg[i].method === 'PUT') {
-
-
-		} else if (cfg[i].method === 'PATCH') {
-
-
-		} else if (cfg[i].method === 'DELETE') {
-
-
-
-		} else {
-			console.log(`unsupported method: ${cfg[i].method}`);
-			process.exit();
-		}
-
-	    }
-
-	} catch (e) {
-		console.log(`error with configuration for: ${cfg[i].path}`);
-		console.log(e);
-	}
+            } else if (cfg[i].method === 'POST') {
+                console.log(`TODO: method '${cfg[i].method}' not supported by altLambda yet`);
+            } else if (cfg[i].method === 'PUT') {
+                console.log(`TODO: method '${cfg[i].method}' not supported by altLambda yet`);
+            } else if (cfg[i].method === 'PATCH') {
+                console.log(`TODO: method '${cfg[i].method}' not supported by altLambda yet`);
+            } else if (cfg[i].method === 'DELETE') {
+                console.log(`TODO: method '${cfg[i].method}' not supported by altLambda yet`);
+            } else {
+                console.log(`unsupported method: ${cfg[i].method}`);
+                process.exit();
+            }
+        } // end for()
+    } catch (e) {
+        console.log(`altLambda->ERROR: with configuration for: ${cfg[i].path}`);
+        console.log(e);
+    }
 }
 
 //
@@ -189,4 +216,3 @@ loadCfg('altlambda.json', app);
 app.listen(port, () => {
   console.log(`altLambda->listening at http://localhost:${port}\n`)
 });
-
