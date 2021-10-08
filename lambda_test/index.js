b7e742eb23d6fdd1d116f7ce35363555deabd14c
Ivan Kluzak <sooper@gmail.com>
2021-10-08 17:18:01 -0500

    other cleanup

--- lambda_test/index.js
+++ lambda_test/index.js
@@ -1,31 +1,37 @@
-
+//
+// Test lambda function:
 var AWS = require('aws-sdk');
 var db = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2' });
 
 async function handler(event) {
 
-	console.log(JSON.stringify(event, null, 2));
+    //
+    // Log the incoming event:
+    console.log(JSON.stringify(event, null, 2));
+    
+    //
+    // Dump the environmental variables:
+    console.log(`ENV: `, JSON.stringify(process.env, null, 2));
 
-	console.log(`ENV: `, JSON.stringify(process.env, null, 2));
-
-	console.log(`HELLO: from lambda_test.handler()`);
-
-
-	var dbres = await db.get({ 
-		TableName: 'sometable',
-		Key : { pk : 'foo' }
-	}).promise();
+    // 
+    // Fetch from a db
+    // var dbres = await db.get({ 
+    //     TableName: 'sometable',
+    //     Key : { pk : 'foo' }
+    // }).promise();
 
 	const res = {
-                statusCode : 200,
-		headers: {
-                	'Access-Control-Allow-Origin': '*'
-                },
-                body : JSON.stringify({ msg: "Hello world!", res: dbres })
-        };
+        statusCode : 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body : JSON.stringify({ msg: "Hello world!", res: dbres })
+    };
 
-	console.log(`res: `, JSON.stringify(res, null, 2));
-
+    // Log our output payload
+    console.log(`res: `, JSON.stringify(res, null, 2));
+    
+    // return it!
 	return res;
 }
 
